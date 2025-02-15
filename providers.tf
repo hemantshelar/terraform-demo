37b7a8b5f0ce8c7a9ecc9b1def5bd59d2656e62a
Hemant <hemant.shelar@gmail.com>
2025-02-16 10:17:49 +1100

    Store tfstate file in azure blob storage.

--- providers.tf
+++ providers.tf
@@ -9,6 +9,12 @@ terraform {
       version = "~>3.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "tfdemo0506"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
