3448669f6ba0207ca8714b4f5e6400899e23507f
Hemant <hemant.shelar@gmail.com>
2025-02-16 08:28:35 +1100

    Service bus in place.

--- variables.tf
+++ variables.tf
@@ -9,3 +9,10 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "The environment for the resources in this example."
+  
+}
