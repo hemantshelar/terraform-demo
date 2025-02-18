a2774b89f855f4249486871e2ea977eb7f310a67
Hemant <hemant.shelar@gmail.com>
2025-02-18 20:29:59 +1100

    output binding working.

--- ResourceGroup/main.tf
+++ ResourceGroup/main.tf
@@ -1,4 +1,26 @@
-resource "azurerm_resource_group" "example" {
-  name     = "rg-${var.base_name}"
+# Purpose: Create a resource group in Azure
+# Usage: rg-dev-tfdemo-aae
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.rg-prefix}-${var.env-prefix}-${var.project-name}-${var.location-prefix}"
   location = "${var.location}"
+  tags = {
+        environment = "${var.environment}"
+    }
 }
--- ResourceGroup/variables.tf
+++ ResourceGroup/variables.tf
@@ -1,8 +1,26 @@
-variable "base_name"{
+//rg-dev-tfdemo-aae
+variable "rg-prefix" {
     type = string
-    description = "Base name for all the resources."
+    description = "Prefix for rg"
+}
+variable "project-name" {
+    type = string
+    description = "Name of the project."
+}
+variable "env-prefix" {
+  type = string
+  description = "Prefix for env"
+}
+variable "location-prefix" {
+    type = string
+    description = "Location prefix for all resources."
 }
 variable "location" {
     type = string
     description = "location for all resources."
+}
+variable "environment" {
+    type = string
+    description = "Environment name."
+    default = "Development"
 }
